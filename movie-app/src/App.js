9658d831c99c0a51198c1b4aaf6dd3d7cb3939f7
Srim <deamoon0801@naver.com>
2020-07-30 19:25:52 +0900

    movie-app 생성

---
+++ movie-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = {
+    isLoading : true,
+    movies : []
+  };
+
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+  }
+
+  async componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading} = this.state;
+  return <div>{isLoading ? "Loading" : "We are ready"}</div>
+  }
+}
+
+export default App;
