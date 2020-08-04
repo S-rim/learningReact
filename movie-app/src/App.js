b256ad9b43bb2c894a1b2ec85e30c8df5c29c73c
Srim <deamoon0801@naver.com>
2020-08-04 15:34:48 +0900

    [ADD] react-router-dom

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,50 +1,17 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie'
-import './App.css'
-import Loading from './LoadingPage'
+import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './routes/Home'
+import About from './routes/About'
+import Navigation from './components/Navigation'
 
-class App extends React.Component {
-  state = {
-    isLoading : true,
-    movies : []
-  };
-
-  getMovies = async () => {
-    const {
-      data : {
-        data : {movies}
-      }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
-    this.setState({movies, isLoading : false});
-  }
-
-  async componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const {isLoading, movies} = this.state;
-    return (<section className="container">
-      {isLoading 
-      ? <Loading></Loading>
-    :  (
-        <div className="movies">
-          {movies.map(movie => (
-          <Movie
-          key={movie.id}
-          id={movie.id} 
-          year={movie.year} 
-          title={movie.title} 
-          summary={movie.summary} 
-          poster={movie.medium_cover_image}
-          genres={movie.genres}>
-        </Movie>
-      ))}
-      </div>
-      )}
-    </section>)
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navigation></Navigation>
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ movie-app/src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import "./Navigation.css"
+
+const Navigation = () => {
+    return (
+        <div>
+            <button className="router-button"><Link to="/">Home</Link></button>
+            <button className="router-button"><Link to="/about">About</Link></button>
+        </div>
+    );
+};
+
+export default Navigation;
