0cbf6c1340ed0efa57e07360898d567ff9576731
Srim <deamoon0801@naver.com>
2021-05-19 22:39:47 +0900

    useRecoilValue -> useRecoilValueLoadable

--- recoil-api-ex/src/App.js
+++ recoil-api-ex/src/App.js
@@ -8,9 +8,7 @@ export default function App() {
       <h1>Random Cat</h1>
       <p>페이지를 새로 고침할 때마다 랜덤한 고양이 사진을 보여줍니다!</p>
       <RecoilRoot>
-        <React.Suspense fallback={null}>
-          <RandomCat />
-        </React.Suspense>
+        <RandomCat />
       </RecoilRoot>
     </div>
   );
--- recoil-api-ex/src/components/RandomCat.js
+++ recoil-api-ex/src/components/RandomCat.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValueLoadable } from "recoil";
 import { randomCat } from "../store/state";
 
 const RandomCat = () => {
-  const photoUrl = useRecoilValue(randomCat);
-  return (
-    <div className="random-cat">
-      <img src={photoUrl} alt="random cat" />
-    </div>
-  );
+  const photoUrl = useRecoilValueLoadable(randomCat);
+  let content = null;
+
+  switch (photoUrl.state) {
+    case "hasValue":
+      content = <img src={photoUrl.contents} alt="random cat" />;
+      break;
+    case "hasError":
+      content = "데이터를 불러오는 중 에러가 발생했습니다.";
+      break;
+    case "loading":
+    default:
+      content = "불러오는 중 ...";
+      break;
+  }
+  return <div className="random-cat">{content}</div>;
 };
 
 export default RandomCat;
